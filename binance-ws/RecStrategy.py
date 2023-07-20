9779ee2a8956e48bee1471ca351638e15b8b9502
LuneZ99 <lune_z@foxmail.com>
2023-07-20 16:57:05 +0800

    update binance strategy

--- binance-ws/RecStrategy.py
+++ binance-ws/RecStrategy.py
@@ -7,7 +7,11 @@ from Handler import SymbolStreamCsvHandler
 class Rec2CsvStrategy(BinanceSyncStrategy):
     handlers: dict[str, SymbolStreamCsvHandler]
 
-    def __init__(self, parent_path, log_file, symbols=None, config_file=None, proxy=None, ws_trace=False, debug=False):
+    def __init__(self, parent_path, log_file=None, symbols=None, config_file=None, proxy=None, ws_trace=False, debug=False):
+
+        if log_file is None:
+            log_file = os.path.join(parent_path, "log.txt")
+
         super().__init__(log_file, config_file, proxy, ws_trace, debug)
 
         if symbols is None:
@@ -50,24 +54,60 @@ class Rec2CsvStrategy(BinanceSyncStrategy):
 
 if __name__ == '__main__':
 
-    symbols_all = ["ethusdt", "btcusdt", "dogeusdt", "xrpusdt", "solusdt", "suiusdt"]
+    # symbols_all = ["ethusdt", "btcusdt", "dogeusdt", "xrpusdt", "solusdt", "suiusdt"]
+    #
+    # s = Rec2CsvStrategy(
+    #     symbols=symbols_all,
+    #     parent_path="./raw_folder",
+    #     proxy=[
+    #         "http://127.0.0.1:7890",
+    #         "http://i.**REMOVED**:7890",
+    #     ],
+    #     ws_trace=False
+    # )
+    #
+    # for _symbol in symbols_all:
+    #     s.subscribe(_symbol, "aggTrade", write_to_log=True)
+    #     s.subscribe(_symbol, "kline_1m", write_to_log=True)
+    #     s.subscribe(_symbol, "depth20@100ms", write_to_log=True)
+    #     s.subscribe(_symbol, "forceOrder", write_to_log=True)
+    #     s.subscribe(_symbol, "bookTicker", write_to_log=True)
+    #
+    # s.run()
+
+    import argparse
+
+    # 创建 ArgumentParser 对象
+    parser = argparse.ArgumentParser(description="Rec2CsvStrategy Command Line Interface")
+
+    # 添加命令行参数
+    parser.add_argument("--symbols", type=str, nargs="+",
+                        default=["ethusdt", "btcusdt", "dogeusdt", "xrpusdt", "solusdt", "suiusdt"],
+                        help="List of symbols to subscribe")
+    parser.add_argument("--parent_path", type=str, default="./raw_folder", help="Parent directory path")
+    parser.add_argument("--proxy", type=str, nargs="+",
+                        default=["http://127.0.0.1:7890", "http://i.**REMOVED**:7890"],
+                        help="List of proxy URLs")
+    parser.add_argument("--log_file", type=str, default="./raw_folder/log.txt", help="Log file path")
+    parser.add_argument("--ws_trace", action="store_true", help="Enable WebSocket trace")
+    parser.add_argument("--subscriptions", type=str, nargs="+",
+                        default=["aggTrade", "kline_1m", "depth20@100ms", "forceOrder", "bookTicker"],
+                        help="List of subscription strings")
+
+    # 解析命令行参数
+    args = parser.parse_args()
 
     s = Rec2CsvStrategy(
-        symbols=symbols_all,
-        parent_path="./raw_folder",
-        proxy=[
-            "http://127.0.0.1:7890",
-            "http://i.**REMOVED**:7890",
-        ],
-        log_file="./raw_folder/log.txt",
-        ws_trace=False
+        symbols=args.symbols,
+        parent_path=args.parent_path,
+        proxy=args.proxy,
+        log_file=args.log_file,
+        ws_trace=args.ws_trace
     )
 
-    for symbol in symbols_all:
-        s.subscribe(symbol, "aggTrade", write_to_log=True)
-        s.subscribe(symbol, "kline_1m", write_to_log=True)
-        s.subscribe(symbol, "depth20@100ms", write_to_log=True)
-        s.subscribe(symbol, "forceOrder", write_to_log=True)
-        s.subscribe(symbol, "bookTicker", write_to_log=True)
+    for _symbol in args.symbols:
+        for subscription in args.subscriptions:
+            s.subscribe(_symbol, subscription, write_to_log=True)
 
     s.run()
+
